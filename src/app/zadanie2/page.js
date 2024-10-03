bc24cb5accdb5ecf21eaf05cd3c40db5416e5835
FZ2161 <filipzalewski44@gmail.com>
2024-10-03 11:02:34 +0200

    fajny if i jakieś rzeczy

--- src/app/zadanie2/page.js
+++ src/app/zadanie2/page.js
@@ -6,17 +6,20 @@ export default function Color() {
         const [color, setColor] = useState("bg-black")
 
         const handleClick = () =>{
-            if(color=="bg-gray-700" || color=="black"){ 
-                setColor("bg-white")
-            }
-            else setColor("bg-gray-700")
+            // if(color=="bg-gray-700" || color=="black"){ 
+            //     setColor("bg-white")
+            // }
+            // else setColor("bg-gray-700") 
+            //////////////////////////////// TO NA DOLE JEST LEPSZE I JEST NA KARTKÓWKE!!!!!!!!
+
+            setColor(color == "bg-black" || color == "bg-gray-700"? "bg-white" : "bg-gray-700" )/* KARTKÓWKA: taki magiczny if */
         }
     return(
         <>
-        <div className={color}>
-            ok
-        </div>  
+        <div className={`h-screen w-full ${color}`}>{/* KARTKÓWKA: połączenie ${} i zwykłego w className */}
         <button onClick={handleClick}>Zmień</button>
+        </div>  
+        
         </>
     )
 }
--- src/components/myMenu.js
+++ src/components/myMenu.js
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Menu() {
     return(
-        <div className="flex justify-center gap-3">
+        <div className="flex justify-center gap-3 text-green-600">
             <Link href="/">Main</Link>
-            |
+            
             <Link href="/strona1">Strona1</Link>
-            |
+            
             <Link href="/zadanie1">Zadanie 1</Link>
-            |
+            
             <Link href="/zadanie2">Zadanie 2</Link>
-            |
+            
             <a href="/strona1">zwykły link</a>
 
         </div>
